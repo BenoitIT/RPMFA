654c107cde8765681aff929f27c953d557e411f1
BenoitIT <benoitnda35@gmail.com>
2024-06-06 08:09:30 +0200

    memebership certificate provision and downloading

--- src/app/member/dashboard/(contents)/certicates.tsx
+++ src/app/member/dashboard/(contents)/certicates.tsx
@@ -2,6 +2,7 @@
 import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { useEffect, useState } from "react";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { Cloudinary } from "cloudinary-core";
 interface certificate {
   memberships: any[];
 }
@@ -10,12 +11,33 @@ interface certificateCard {
   certificateLink: string;
   onClick: (val: string) => void;
   currentPreview: string;
+  onDownload: () => void;
 }
 const Certificates = ({ memberships }: certificate) => {
+  const cld = new Cloudinary({ cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME });
   const [certificate, setCertificate] = useState("");
   useEffect(() => {
     setCertificate(memberships[0]?.membershipCertificate);
   }, [memberships[0]?.membershipCertificate]);
+  const handleDownload =async () => {
+    const imageUrl = cld.url(certificate, {
+      width: 800,
+      height: 600,
+      quality: "auto",
+      fetch_format: "auto",
+    });
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const contentType = blob.type.split('/')[1]; 
+    const blobUrl = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = blobUrl;
+    link.download = `certificate.${contentType}`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(blobUrl);
+  };
   if (memberships) {
     return (
       <div className="w-full flex flex-row gap-[4px] py-[3vh] px-[2vw] bg-white rounded min-h-[80vh]">
@@ -27,6 +49,7 @@ const Certificates = ({ memberships }: certificate) => {
               certificateLink={membership?.membershipCertificate}
               onClick={setCertificate}
               currentPreview={certificate}
+              onDownload={handleDownload}
             />
           ))}
         </div>
@@ -58,6 +81,7 @@ const Certifcate = ({
   certificateLink,
   currentPreview,
   onClick,
+  onDownload,
 }: certificateCard) => {
   return (
     <div className="flex flex-col gap-2">
@@ -67,9 +91,21 @@ const Certifcate = ({
         } hover:border-2 border-blue-300 hover:cursor-pointer`}
         onClick={() => onClick(certificateLink)}
       >
-        <MdOutlineFileDownload className=" text-blue-600 text-2xl" />
+        <MdOutlineFileDownload
+          className=" text-blue-600 text-2xl"
+          onClick={() => {
+            onClick(certificateLink);
+            onDownload();
+          }}
+        />
       </div>
       <h2 className="text-sm ml-2">Certificate {name}</h2>
     </div>
   );
 };
+// function getImage(
+//   publicId: any,
+//   arg1: { width: number; height: number; quality: number }
+// ) {
+//   throw new Error("Function not implemented.");
+// }
