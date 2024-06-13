ea5859d7d8dad6543f81456c49c2d98c022b4cc0
BenoitIT <benoitnda35@gmail.com>
2024-06-13 20:31:44 +0200

    enforcing validation issues

--- src/app/(components)/inputs/Inputs.tsx
+++ src/app/(components)/inputs/Inputs.tsx
@@ -6,7 +6,8 @@ interface PrimaryInputProps {
   value?: string;
   placeholder: string;
   changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  required?:boolean;
+  required?: boolean;
+  disabled?: boolean;
 }
 export const PrimaryInput = ({
   label,
@@ -15,7 +16,8 @@ export const PrimaryInput = ({
   changeHandler,
   placeholder,
   value,
-  required
+  required,
+  disabled,
 }: PrimaryInputProps) => {
   return (
     <div>
@@ -23,7 +25,14 @@ export const PrimaryInput = ({
         htmlFor={name}
         className="flex mb-2 text-sm font-medium text-gray-900 gap-4"
       >
-        <span>{label}</span><span className={required!=null?"hidden":"text-red-400 text-xs mt-[2.5px]"}>Required</span>
+        <span>{label}</span>
+        <span
+          className={
+            required != null ? "hidden" : "text-red-400 text-xs mt-[2.5px]"
+          }
+        >
+          Required
+        </span>
       </label>
       <input
         type={type}
@@ -33,6 +42,7 @@ export const PrimaryInput = ({
         onChange={changeHandler}
         className="bg-gray-1 outline-none text-gray-900 sm:text-sm rounded-lg block w-full p-2 md:p-2.5 placeholder:text-sm"
         placeholder={placeholder}
+        disabled={disabled && disabled}
       />
       <p
         className={
