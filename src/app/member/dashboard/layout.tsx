607303a28850c73f585c6a60636f1029603a01e1
BenoitIT <benoitnda35@gmail.com>
2024-06-24 15:18:07 +0200

    enforcing security concerns

--- src/app/member/dashboard/layout.tsx
+++ src/app/member/dashboard/layout.tsx
@@ -6,25 +6,33 @@ import {
   membersDashboardLinks,
 } from "@/app/dashboard/(components)/links";
 import Navbar from "@/app/dashboard/(components)/Navabar";
+import { auth } from "@/auth";
+import Redirector from "@/app/(components)/redirector/Redirector";
 
-const SidebarLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="flex w-full">
-      <div className="w-64 max-sm:hidden max-md:w-52">
-      <div className="fixed w-64 max-sm:hidden max-md:w-52 ">
-        <Sidebar
-          DashboardLinks={membersDashboardLinks}
-          HomeMenu={memberHomeMenu}
-          SidebarFooterMenu={memberProfileAndSupportLinks}
-        />
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session: any = await auth();
+  const role: string = session?.user?.role;
+  if (role == "member") {
+    return (
+      <div className="flex w-full">
+        <div className="w-64 max-sm:hidden max-md:w-52">
+          <div className="fixed w-64 max-sm:hidden max-md:w-52 ">
+            <Sidebar
+              DashboardLinks={membersDashboardLinks}
+              HomeMenu={memberHomeMenu}
+              SidebarFooterMenu={memberProfileAndSupportLinks}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          <div className="w-full p-4 mt-14">{children}</div>
         </div>
       </div>
-      <div className="flex flex-col flex-1">
-        <Navbar />
-        <div className="w-full p-4 mt-14">{children}</div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <Redirector />;
+  }
 };
 
-export default SidebarLayout;
+export default Layout;
