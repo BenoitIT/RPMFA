b6e07793587d85a1fd7561af568345306dbd42b5
BenoitIT <benoitnda35@gmail.com>
2024-09-30 06:41:11 +0800

    announcements

--- src/app/announcements/(contents)/AnnouncementContents.tsx
+++ src/app/announcements/(contents)/AnnouncementContents.tsx
@@ -11,7 +11,7 @@ const AnnouncementContents = ({ announcements }: AnnouncmentProps) => {
   return (
     <>
       {Array.isArray(activeAnnouncements) && activeAnnouncements.length > 0 ? (
-        <div className="w-full mx-auto min-h-[66vh]">
+        <div className="w-full mx-auto min-h-[66vh] px-6 lg:px-12 xl:px-14">
           <div className="flex justify-between my-8 gap-y-3 max-sm:flex-col">
             <h1 className="text-xl font-medium text-blue-1">
               Announcements
